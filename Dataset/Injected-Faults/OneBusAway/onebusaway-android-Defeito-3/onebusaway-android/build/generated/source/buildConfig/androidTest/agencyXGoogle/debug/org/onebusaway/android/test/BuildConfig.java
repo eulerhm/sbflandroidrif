28b599d358b9a0a489b7f2c8d09a6a36c3696030
eulerhm <eulerhm@dcc.ufmg.br>
2024-11-09 22:57:52 -0300

    Added Files

---
+++ Dataset/Injected-Faults/OneBusAway/onebusaway-android-Defeito-3/onebusaway-android/build/generated/source/buildConfig/androidTest/agencyXGoogle/debug/org/onebusaway/android/test/BuildConfig.java
@@ -0,0 +1,73 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.onebusaway.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.agencyx.android.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "agencyXGoogle";
+  public static final String FLAVOR_brand = "agencyX";
+  public static final String FLAVOR_platform = "google";
+  // Field from product flavor: agencyX
+  public static final int ARRIVAL_INFO_STYLE = 0;
+  // Field from default config.
+  public static final String CI = "null";
+  // Field from product flavor: agencyX
+  public static final double FIXED_REGION_BOUNDS_LAT = 0;
+  // Field from product flavor: agencyX
+  public static final double FIXED_REGION_BOUNDS_LAT_SPAN = 0;
+  // Field from product flavor: agencyX
+  public static final double FIXED_REGION_BOUNDS_LON = 0;
+  // Field from product flavor: agencyX
+  public static final double FIXED_REGION_BOUNDS_LON_SPAN = 0;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_CONTACT_EMAIL = null;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_ENROLL_PARTICIPANTS_IN_STUDY = false;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_LANG = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_NAME = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OBA_BASE_URL = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OPEN311_API_KEY = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OPEN311_BASE_URL = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OPEN311_JURISDICTION_ID = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OTP_BASE_URL = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_OTP_CONTACT_EMAIL = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_PAYMENT_ANDROID_APP_ID = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_PAYMENT_WARNING_BODY = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_PAYMENT_WARNING_TITLE = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_SIRI_BASE_URL = null;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_STOP_INFO_URL = null;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_SUPPORTS_OBA_DISCOVERY_APIS = true;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_SUPPORTS_OBA_REALTIME_APIS = true;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_SUPPORTS_OTP_BIKESHARE = false;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_SUPPORTS_SIRI_REALTIME_APIS = false;
+  // Field from product flavor: agencyX
+  public static final boolean FIXED_REGION_TRAVEL_BEHAVIOR_DATA_COLLECTION = false;
+  // Field from product flavor: agencyX
+  public static final String FIXED_REGION_TWITTER_URL = null;
+  // Field from product flavor: agencyX
+  public static final String PELIAS_API_KEY = "";
+  // Field from product flavor: agencyX
+  public static final boolean USE_FIXED_REGION = false;
+  // Field from product flavor: agencyX
+  public static final boolean USE_PELIAS_GEOCODING = true;
+}
